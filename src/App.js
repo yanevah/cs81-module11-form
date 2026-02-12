4616adcfe9549e8ccb29aa03dcdee681b0feb7ea
Nina <janev_nina_alexandra@student.smc.edu>
2026-02-12 10:07:46 -0800

    Integrate ContactForm component into main App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactForm from './ContactForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Contact Form App</h1>
+      <ContactForm />
     </div>
   );
 }
